c3adfc2b2fc9d591f3bb0792fe0199b8ee14addc
darshan033 <89646777+darshan033@users.noreply.github.com>
2021-08-28 13:15:48 +0530

    Update Activity_03.py

---
+++ Activity_03.py
@@ -0,0 +1,14 @@
+>>> string1="Good "
+>>> string2="morning\n"
+>>> concatenated_string=string1+string2
+>>> print(concatenated_string)
+Good morning
+
+>>> print(concatenated_string*5)
+Good morning
+Good morning
+Good morning
+Good morning
+Good morning
+
+>>> 
