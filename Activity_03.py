a24ea0a1306abe598f21b85d4b3eb510a9dfa7b2
darshan033 <89646777+darshan033@users.noreply.github.com>
2021-08-29 14:37:36 +0530

    Update Activity_03.py

--- Activity_03.py
+++ Activity_03.py
@@ -1,5 +1,5 @@
 string1 = input("Enter the first string :")
-string2 = input("Enter the second string")
+string2 = input("Enter the second string :\n")
 concatenated_string = string1 + string2
 print(concatenated_string)
 print(concatenated_string*5)
