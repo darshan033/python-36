dfb3614515c3d5bf6e0759ac04149fccd66be16c
darshan033 <89646777+darshan033@users.noreply.github.com>
2021-08-29 14:25:49 +0530

    Update Activity_05.py

--- Activity_05.py
+++ Activity_05.py
@@ -1,5 +1,7 @@
-list=(2,4,9,1,5)
+input_line = input("Enter five numbers seperated by space:")
+number_string_list = input_line.split()
 sum=0
-for i in list:
-	sum=sum+int(i)
-	print(sum)
+for i in number_string_list:
+    sum=sum+int(i)
+    print(sum)
+
