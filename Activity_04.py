143d91e5e7ae1fec3232c9ad64f3ef84188501e8
darshan033 <89646777+darshan033@users.noreply.github.com>
2021-08-29 20:53:54 +0530

    Update Activity_04.py

--- Activity_04.py
+++ Activity_04.py
@@ -1,13 +1,9 @@
-a=int("Enter the first number to be added")
-b=int("Enter the second number to be added")
+a=int(input("Enter the first number to be added"))
+b=int(input("Enter the second number to be added"))
 c=a+b
 print("The sum of %d and %d is %d\n"%(a,b,c))
- 
-  a=int(input("Enter the first number to be added"))
-b=int(input("Enter the second number to be added"))
+
 sum=a+b
 print(sum)
 
- a=int(input("Enter the first number to be added"))
-b=int(input("Enter the second number to be added"))
 print(a+b)
